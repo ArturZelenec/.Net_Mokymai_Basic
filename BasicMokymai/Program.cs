3d609afc07087dfb84af98c0aa14cbe18e805f90
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-06-06 21:28:14 +0300

    Console

--- BasicMokymai/Program.cs
+++ BasicMokymai/Program.cs
@@ -4,26 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Artur");
+            //Console.WriteLine("Artur");
 
-            Console.Write("išvedimas ");
-            Console.Write("vienoje ");
-            Console.Write("eiluteje ");
-            Console.WriteLine();
-            Console.Write("tekstas kitoje eiluteje");
-            Console.WriteLine("tekstas ");
+            //Console.Write("išvedimas ");
+            //Console.Write("vienoje ");
+            //Console.Write("eiluteje ");
+            //Console.WriteLine();
+            //Console.Write("tekstas kitoje eiluteje");
+            //Console.WriteLine("tekstas ");
 
-            Console.WriteLine("_______________________");
-            Console.WriteLine("išvedima "+ "vienoje "+ "eilueteje ");
-            Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eiluteje");
-            Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eiluteje"}");
-            Console.WriteLine("_______________________");
+            //Console.WriteLine("_______________________");
+            //Console.WriteLine("išvedima "+ "vienoje "+ "eilueteje ");
+            //Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eiluteje");
+            //Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eiluteje"}");
+            //Console.WriteLine("_______________________");
 
-            Console.WriteLine("tekstas idedamas i \"kabutes\"");
-            Console.WriteLine("tekstas \nidedamas \ni \nkabutes");
-            Console.WriteLine("tekstas \tidedamas \ti \tkabutes");
+            //Console.WriteLine("tekstas idedamas i \"kabutes\"");
+            //Console.WriteLine("tekstas \nidedamas \ni \nkabutes");
+            //Console.WriteLine("tekstas \tidedamas \ti \tkabutes");
 
+            //Console.WriteLine("--------Press any key to continue-----------");
+            //Console.ReadKey();
+            //Console.Clear();
 
+            //Console.WriteLine("Iveskite savo varda, o as ji pakartosiu");
+            //Console.WriteLine($"Jusu vardas {Console.ReadLine()}");
+
+            //Console.WriteLine("Iveskite raide");
+            //var key = Console.ReadKey();
+            //Console.WriteLine($"ivestas simbolis {key.KeyChar}");
+            //Console.WriteLine($"ivestas simbolis {key.Key}");
+            //Console.WriteLine($"ivestas simbolis {(int)key.KeyChar}");
+            
+            Console.WriteLine("Iveskite savo varda, o as atspesiu pirma raide");
+            Console.WriteLine($"o stai mano pirma raide {Console.ReadLine()[0]}");
+            
 
 
 
