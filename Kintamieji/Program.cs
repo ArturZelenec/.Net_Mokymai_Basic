69e01d41986741da3125bebda54b5d0c0282fb46
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-06-08 22:08:21 +0300

    Kintamieji uzdaviniai

---
+++ Kintamieji/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki kintamieji");
+            //Skaiciu kintamieji
+            //sveiku skaiciu kintamieji
+            byte mazasSkaicius = 2; //nuo 0 iki 255 tai max skaicius
+            short trumpasSkaicius = 2; //nuo -32768 iki 32767
+            int skaicius = 2; //nuo -2.147.483.648 iki 2.147.483.647
+            int maksimalusIntSkaitmuo = int.MaxValue;
+            int minimalusIntSkaitmuo = int.MinValue;
+
+            int? skaiciusKurisGaliButiNull;
+            skaiciusKurisGaliButiNull = null;
+
+            Nullable<int> skaiciusKurisGaliButi = null;
+
+            uint tikTeigimasSkaicius = 2; //tik teigiamas skaicius reiskia [uint]
+            int? sk = null; //[?] leidzia kintamaji padaryti nulabel
+            Console.WriteLine(sk);
+
+            float maziausiasTrupmeninis = 8.5f;
+            double trupmeninis = 8.5;
+            decimal didziausiasTrupmeninis = 8.5m;
+
+            double a = 6.022e23; // taip uzrasoma 6.022x10^23
+            double b = 522_1_000.000_001; //the digit separator
+
+            //daugibine deklaracija
+            int deklaracija, deklaracija1, deklaracija2;
+            deklaracija = deklaracija1 = deklaracija2 = 100; //visem bus priskirta reiksme 100
+
+            Console.WriteLine("Loginiai kintamieji");
+            bool teisybe = true; // reprezentuoja reisybe. Gali buti irasyta true ir false
+            bool neteisybe = false;
+            bool? nullableLoginis = null;
+
+            Console.WriteLine("Char kintamieji");
+            char raide = 'a'; // yra sveiko skaitmens tipas, nors atrodo kad talpina raide
+
+
+            
+
+        }
+    }
+}
---
+++ Kintamieji_Uzdaviniai/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Kintamieji_Uzdaviniai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mokyklosPavadinimas = "CodeAcademy";
+            string kursoPavadinimas = ".Net";
+            int studentuSkaicius = 19;
+            DateTime siosDienosData = DateTime.Now;
+            Console.WriteLine($"\nMOKYKLOS PAVADINIMAS: {mokyklosPavadinimas} \nKURSO PAVADINIMAS: {kursoPavadinimas} \nSTUDENTU SKAICIUS: {studentuSkaicius} \nDATA: {siosDienosData}");
+
+
+            DateTime kursoPradzia = new DateTime(2022, 05, 30);
+            DateTime kursoPabaiga = new DateTime(2022, 12, 01);
+            var visoDienu = (siosDienosData - kursoPradzia) / 7;
+            var darboDienu = visoDienu * 5;
+            Console.WriteLine($"darboDienu {darboDienu.Days}");
+
+
+            string text = "text";
+            int number = 1;
+            bool logika = true;
+            Console.WriteLine($"{text} {number} {logika}");
+
+
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+            Console.WriteLine($"{num1} {num2} {num3}");
+            Console.WriteLine(num1 + " " + num2 + " " + num3);
+        }
+    }
+}
