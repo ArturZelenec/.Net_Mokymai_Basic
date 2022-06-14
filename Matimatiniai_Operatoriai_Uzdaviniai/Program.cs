d19b31084613e85bfdcaf394e9f82b41c0723444
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-06-14 23:26:18 +0300

    savarankiskas darbas, termometras done

--- Matimatiniai_Operatoriai_Uzdaviniai/Program.cs
+++ Matimatiniai_Operatoriai_Uzdaviniai/Program.cs
@@ -28,13 +28,31 @@ sukurkite naują kintamajį short ir prskirkite didžiausią reikšmę
 - ir pridėkite maksimalų int skaičių
 */
 
-long longSkaicius = long.MaxValue;
-short shortSkaicius = short.MaxValue;
-var dalyba = longSkaicius / shortSkaicius;
-Console.WriteLine($"dalyba = longSkaicius/shortSkaicius = {longSkaicius/shortSkaicius}");
-var skirtumas = dalyba - longSkaicius;
-Console.WriteLine($"skirtumas = dalyba - longSkaicius = {skirtumas}");
-var suma = skirtumas + int.MaxValue;
-Console.WriteLine($"suma = skirtumas + intMaxValue = {suma}");
+//long longSkaicius = long.MaxValue;
+//short shortSkaicius = short.MaxValue;
+//var dalyba = longSkaicius / shortSkaicius;
+//Console.WriteLine($"dalyba = longSkaicius/shortSkaicius = {longSkaicius/shortSkaicius}");
+//var skirtumas = dalyba - longSkaicius;
+//Console.WriteLine($"skirtumas = dalyba - longSkaicius = {skirtumas}");
+//var suma = skirtumas + int.MaxValue;
+//Console.WriteLine($"suma = skirtumas + intMaxValue = {suma}");
 
-Console.WriteLine($"rezultatas = {(longSkaicius/shortSkaicius) - longSkaicius + int.MaxValue}");
+//Console.WriteLine($"rezultatas = {(longSkaicius/shortSkaicius) - longSkaicius + int.MaxValue}");
+
+//PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
+//VISUS ĮVESTUS išskyrus 1 PAKEISKITE Į 0 IR GAUTĄ SKAIČIŲ PADALINKITE IŠ 2
+//PVZ BUVO ĮVESTA 12345
+//REZULTATAS 10000/2=5000
+//<HINT> naudokite replace
+//Console.WriteLine("iveskite skaiciu");
+//double sk = Convert.ToDouble(Console.ReadLine().Replace("2", "0").Replace("3","0").Replace("4","0").Replace("5","0"));
+//Console.WriteLine($"rezultatas = {sk/2}");
+
+
+//PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI SVEIKĄ SKAIČIŲ
+//VIENOJE EILUTĖJE IŠVESKITE ŠĮ SKAIČIŲ 5 KARTUS VIS PADIDINAMI VIENETU.
+//PVZ BUVO ĮVESTA 5
+//REZULTATAS 6 7 8 9 10
+var sk1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"rezultatas {++sk1} {++sk1} {++sk1} {++sk1} {++sk1}");
---
+++ Savarankiskas_Darbas_Termometras_Atstumas/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("Iveskite 1 skaičių - temperatūrą pagal Celsijų");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius * 9) / 5 + 32;
+double kelvin = celsius + 273;
+double backToCelsiusFromFahrenheit = (fahrenheit - 32) * 5 / 9;
+double backToCelsiusFromKelvin = kelvin - 273;
+double fahrenheitToKelvin = 273 + (fahrenheit - 32) * 5 / 9;
+Console.WriteLine($"Temperatūrą pagal farenheitą yra {fahrenheit}");
+Console.WriteLine($"Temperatūrą pagal kelviną yra {kelvin}");
+Console.WriteLine($"Patikra ar sutampa fahrenheit su įvestu celsius = {celsius == backToCelsiusFromFahrenheit}");
+Console.WriteLine($"Patikra ar sutampa kelvin su įvestu celsius = {celsius == backToCelsiusFromKelvin}");
+Console.WriteLine($"Patikra ar sutampa kelvin su fahrenheit = {kelvin == fahrenheitToKelvin}");
+
+Console.WriteLine("_________________");
+Console.WriteLine("  ^F          ^C  ");
+Console.WriteLine($"| {fahrenheit + 54}  | |    {celsius + 30} |");
+Console.WriteLine($"| {fahrenheit + 45}   | |    {celsius + 25} |");
+Console.WriteLine($"| {fahrenheit + 36}   | |    {celsius + 20} |");
+Console.WriteLine($"| {fahrenheit + 27}   | |    {celsius + 15} |");
+Console.WriteLine($"| {fahrenheit + 18}   | |    {celsius + 10} |");
+Console.WriteLine($"| {fahrenheit + 9}   | |    {celsius + 5} |");
+Console.WriteLine($"| {fahrenheit}   |X|    {celsius} |");
+Console.WriteLine($"| {fahrenheit - 9}   |X|     {celsius - 5} |");
+Console.WriteLine($"| {fahrenheit - 18}   |X|     {celsius - 10} |");
+Console.WriteLine($"| {fahrenheit - 27}   |X|    {celsius - 15} |");
+Console.WriteLine($"| {fahrenheit - 36}   |X|   {celsius - 20} |");
+Console.WriteLine($"| {fahrenheit - 45}    |X|   {celsius - 25} |");
+Console.WriteLine($"| {fahrenheit - 54}   |X|   {celsius - 30} |");
+Console.WriteLine($"| {fahrenheit - 63}  |X|   {celsius - 35} |");
+Console.WriteLine($"| {fahrenheit - 72}  |X|   {celsius - 40} |");
+Console.WriteLine($"| {fahrenheit - 81}  |X|   {celsius - 45} |");
+Console.WriteLine($"| {fahrenheit - 90}  |X|   {celsius - 50} |");
+Console.WriteLine("|________________|");
+
