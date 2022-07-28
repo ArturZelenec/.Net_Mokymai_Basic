6fe9870864af102286f4ea13e65a950ffa44a589
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-07-29 00:37:06 +0300

    aaa

--- HangMan_Zaidimas/HangMan_Zaidimas/Program.cs
+++ HangMan_Zaidimas/HangMan_Zaidimas/Program.cs
@@ -2,179 +2,329 @@
 
 namespace HangMan_Zaidimas
 {
-    
-    
-        internal class Program
+
+
+    internal class Program
+    {
+        public static List<char> atspetosRaides = new List<char>();
+        public static List<char> neraRaidziuZodije = new List<char>();
+        public static bool win = false;
+        public static int atskleistosRaides = 0;
+        public static char spejimas;
+        public static string input;
+        public static int spejimai = 7;
+
+        static void Main(string[] args)
         {
-
-
-            static void Main(string[] args)
+            while (true)
             {
-                while (true)
+                Menu();
+                string meniuPasirinkimas = Console.ReadLine();
+                switch (meniuPasirinkimas)
                 {
-                    Menu();
-                    string meniuPasirinkimas = Console.ReadLine();
-                    switch (meniuPasirinkimas)
-                    {
-                        case "1":
-                            Vardai();
-                            break;
-                        case "2":
-                            LietuvosMiestai();
-                            break;
-                        case "3":
-                            Valstybes();
-                            break;
-                        case "4":
-                            Kita(7);
-                            break;
-                        default:
-                            Console.WriteLine("Nera tokios kategorijos");
-                            break;
-                    }
+                    case "1":
+                        Vardai();
+                        break;
+                    case "2":
+                        LietuvosMiestai();
+                        break;
+                    case "3":
+                        Valstybes();
+                        break;
+                    case "4":
+                        Kita(spejimai);
+                        break;
+                    default:
+                        Console.WriteLine("Nera tokios kategorijos");
+                        break;
                 }
             }
+        }
+        private static void Kita(int spejimai)
+        {
+            string[] kita = { "Abrakadabra", "Filamas", "Programuotojas", "Alus", "Lapas", "Kompiuteris" };
 
-            private static void Kita(int spejimai)
+
+            string randomZodis = RandomZodis(kita);
+            //string pasleptasZodis = PasleptasZodis(randomZodis);
+            //var suliginimas = pasleptasZodis.ToUpper();
+
+            string suliginimas = randomZodis.ToUpper();
+            StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
+            for (int i = 0; i < randomZodis.Length; i++)
             {
-                string[] kita = { "Abrakadabra", "Filamas", "Programuotojas", "Alus", "Lapas", "Kompiuteris" };
+                pasleptasZodis.Append('-');
+            }
 
+            while (!win && spejimai > 0) // sukam while kol nelaimesim arba kol nebaigsis ejimai
+            {
+                Console.Write("Iveskite raide:");
+                input = Console.ReadLine().ToUpper(); 
+                spejimas = input[0];
 
-                //string randomZodis = RandomZodis(kita);
-                //string pasleptasZodis = PasleptasZodis(randomZodis);
-                //var suliginimas = pasleptasZodis.ToUpper();
-
-
-                Random rand = new Random();
-                string randomZodis = kita[rand.Next(0, kita.Length)];
-                string suliginimas = randomZodis.ToUpper();
-                StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
-                for (int i = 0; i < randomZodis.Length; i++)
+                if (atspetosRaides.Contains(spejimas))
                 {
-                    pasleptasZodis.Append('-');
+                    Console.WriteLine($"Atspejot \"{spejimas}\" anksciau");
+                    continue;
                 }
-
-                List<char> atspetosRaides = new List<char>();
-                List<char> neraRaidziuZodije = new List<char>();
-                bool win = false;
-                int atskleistosRaides = 0;
-                char spejimas;
-                string input;
-                while (!win && spejimai > 0)
+                else if (neraRaidziuZodije.Contains(spejimas))
                 {
-                    Console.Write("Iveskite raide:");
+                    Console.WriteLine($"Jau bandita raide \"{spejimas}\"");
+                    continue;
+                }
+                if (suliginimas.Contains(spejimas))  // darant atskirus metodus, neieina i sita if
+                {
+                    atspetosRaides.Add(spejimas);
 
-                    input = Console.ReadLine().ToUpper();
-                    spejimas = input[0];
-
-
-                    if (atspetosRaides.Contains(spejimas))
+                    for (int i = 0; i < randomZodis.Length; i++)
                     {
-                        Console.WriteLine($"Atspejot \"{spejimas}\" anksciau");
-                        continue;
-                    }
-                    else if (neraRaidziuZodije.Contains(spejimas))
-                    {
-                        Console.WriteLine($"Jau bandita raide \"{spejimas}\"");
-                        continue;
-                    }
-                    if (suliginimas.Contains(spejimas))  // darant atskirus metodus, neieina i sita if
-                    {
-                        atspetosRaides.Add(spejimas);
-
-                        for (int i = 0; i < randomZodis.Length; i++)
+                        if (suliginimas[i] == spejimas)
                         {
-                            if (suliginimas[i] == spejimas)
-                            {
-
-                                pasleptasZodis[i] = randomZodis[i];  // darant metodus meta klaida "not null"
-                                atskleistosRaides++;
-                            }
+                            //var zodis = pasleptasZodis;
+                            //var rezultatas = zodis.Substring(0, i - 1) + randomZodis[i] + zodis.Substring(i, zodis.Length);
+                            pasleptasZodis[i] = randomZodis[i];  // darant metodus meta klaida "not null"
+                            atskleistosRaides++;
                         }
-                        if (atskleistosRaides == randomZodis.Length)
-                            win = true;
+                        //if (suliginimas[i] == spejimas)
+                        //{
+                        //    pasleptasZodis = pasleptasZodis.Substring(0, i - 1) + randomZodis[i] + pasleptasZodis.Substring(i + 1, randomZodis.Length);
+                        //    atskleistosRaides++;
+                        //}
                     }
-                    else
-                    {
-                        neraRaidziuZodije.Add(spejimas);
-                        Console.WriteLine($"\"{spejimas}\" nera sitame zodije");
-                        spejimai--;
-                        Console.WriteLine($"{spejimai} bandimu liko");
-                    }
-                    Console.WriteLine(pasleptasZodis.ToString());
-                }
-
-                if (win)
-                {
-                    Console.WriteLine($"Jus atspejot!");
-
+                    if (atskleistosRaides == randomZodis.Length)
+                        win = true;
                 }
                 else
                 {
-                    Console.WriteLine($"Jus pralaimejot! \n Zodis yra {randomZodis}");
+                    neraRaidziuZodije.Add(spejimas);
+                    Console.WriteLine($"\"{spejimas}\" nera sitame zodije");
+                    spejimai--;
+                    Console.WriteLine($"{spejimai} bandimu liko");
                 }
-                Console.ReadKey();
+                Console.WriteLine(pasleptasZodis.ToString());
             }
-
-
-
-
-            private static void Valstybes()
+            Rezultatas(win, randomZodis);
+            Console.ReadKey();
+            Reset();
+            Console.Clear();
+        }
+        private static void Valstybes()
+        {
+            string[] valstybes = { "Vilnius", "Kaunas", "Taurage", "Klaipeda", "Telsiai", "Utena" };
+            string randomZodis = RandomZodis(valstybes);
+            string suliginimas = randomZodis.ToUpper();
+            StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
+            for (int i = 0; i < randomZodis.Length; i++)
             {
-                string[] valstybes = { "Vilnius", "Kaunas", "Taurage", "Klaipeda", "Telsiai", "Utena" };
-
+                pasleptasZodis.Append('-');
             }
-
-            private static void LietuvosMiestai()
+            while (!win && spejimai > 0)
             {
-                string[] lietuvosMiestai = { "Vilnius", "Kaunas", "Taurage", "Klaipeda", "Telsiai", "Utena" };
-                ;
-            }
+                Console.Write("Iveskite raide:");
+                input = Console.ReadLine().ToUpper();
+                spejimas = input[0];
 
-            private static void Vardai()
-            {
-                string[] vardai = { "Artur", "Romuald", "Juozas", "Dalia", "Diana", "Tadas" };
-
-            }
-
-            static void Menu()
-            {
-                Console.WriteLine("Pasirinkite tema:"
-                                + "\n-----------------------------------"
-                                + "\n1. VARDAI"
-                                + "\n2. LIETUVOS MIESTAI"
-                                + "\n3. VALSTYBES"
-                                + "\n4. KITA");
-            }
-
-            //public static string RandomPasleptasZodis(string[] sarasas) // podelit na dve casti random v otdelnyi metod i skrytost v otdelnyi metod
-            //{
-            //    Random rand = new Random();
-            //    string randomZodis = sarasas[rand.Next(sarasas.Length)];
-            //    StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
-            //    for (int i = 0; i < randomZodis.Length; i++)
-            //    {
-            //        pasleptasZodis.Append('-');
-            //    }
-            //    return pasleptasZodis.ToString();
-            //}
-
-            public static string RandomZodis(string[] sarasas)
-            {
-                Random rand = new Random();
-                string randomZodis = sarasas[rand.Next(0, sarasas.Length)];
-                return randomZodis;
-            }
-
-            public static string PasleptasZodis(string zodis)
-            {
-                StringBuilder pasleptasZodis = new StringBuilder(zodis.Length);
-                for (int i = 0; i < zodis.Length; i++)
+                if (atspetosRaides.Contains(spejimas))
                 {
-                    pasleptasZodis.Append('-');
+                    Console.WriteLine($"Atspejot \"{spejimas}\" anksciau");
+                    continue;
                 }
-                return pasleptasZodis.ToString();
+                else if (neraRaidziuZodije.Contains(spejimas))
+                {
+                    Console.WriteLine($"Jau bandita raide \"{spejimas}\"");
+                    continue;
+                }
+                if (suliginimas.Contains(spejimas))  
+                {
+                    atspetosRaides.Add(spejimas);
+
+                    for (int i = 0; i < randomZodis.Length; i++)
+                    {
+                        if (suliginimas[i] == spejimas)
+                        {
+                            pasleptasZodis[i] = randomZodis[i];  
+                            atskleistosRaides++;
+                        }
+                    }
+                    if (atskleistosRaides == randomZodis.Length)
+                        win = true;
+                }
+                else
+                {
+                    neraRaidziuZodije.Add(spejimas);
+                    Console.WriteLine($"\"{spejimas}\" nera sitame zodije");
+                    spejimai--;
+                    Console.WriteLine($"{spejimai} bandimu liko");
+                }
+                Console.WriteLine(pasleptasZodis.ToString());
             }
+            Rezultatas(win, randomZodis);
+            Console.ReadKey();
+            Reset();
+            Console.Clear();
+
+        }
+
+        private static void LietuvosMiestai()
+        {
+            string[] lietuvosMiestai = { "Vilnius", "Kaunas", "Taurage", "Klaipeda", "Telsiai", "Utena" };
+            string randomZodis = RandomZodis(lietuvosMiestai);
+            string suliginimas = randomZodis.ToUpper();
+            StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
+            for (int i = 0; i < randomZodis.Length; i++)
+            {
+                pasleptasZodis.Append('-');
+            }
+            while (!win && spejimai > 0)
+            {
+                Console.Write("Iveskite raide:");
+                input = Console.ReadLine().ToUpper();
+                spejimas = input[0];
+                if (atspetosRaides.Contains(spejimas))
+                {
+                    Console.WriteLine($"Atspejot \"{spejimas}\" anksciau");
+                    continue;
+                }
+                else if (neraRaidziuZodije.Contains(spejimas))
+                {
+                    Console.WriteLine($"Jau bandita raide \"{spejimas}\"");
+                    continue;
+                }
+                if (suliginimas.Contains(spejimas))
+                {
+                    atspetosRaides.Add(spejimas);
+
+                    for (int i = 0; i < randomZodis.Length; i++)
+                    {
+                        if (suliginimas[i] == spejimas)
+                        {
+                            pasleptasZodis[i] = randomZodis[i];
+                            atskleistosRaides++;
+                        }
+                    }
+                    if (atskleistosRaides == randomZodis.Length)
+                        win = true;
+                }
+                else
+                {
+                    neraRaidziuZodije.Add(spejimas);
+                    Console.WriteLine($"\"{spejimas}\" nera sitame zodije");
+                    spejimai--;
+                    Console.WriteLine($"{spejimai} bandimu liko");
+                }
+                Console.WriteLine(pasleptasZodis.ToString());
+            }
+            Rezultatas(win, randomZodis);
+            Console.ReadKey();
+            Reset();
+            Console.Clear();
+        }
+
+        private static void Vardai()
+        {
+            string[] vardai = { "Artur", "Romuald", "Juozas", "Dalia", "Diana", "Tadas" };
+            string randomZodis = RandomZodis(vardai);
+            string suliginimas = randomZodis.ToUpper();
+            StringBuilder pasleptasZodis = new StringBuilder(randomZodis.Length);
+            for (int i = 0; i < randomZodis.Length; i++)
+            {
+                pasleptasZodis.Append('-');
+            }
+            while (!win && spejimai > 0)
+            {
+                Console.Write("Iveskite raide:");
+                input = Console.ReadLine().ToUpper();
+                spejimas = input[0];
+
+                if (atspetosRaides.Contains(spejimas))
+                {
+                    Console.WriteLine($"Atspejot \"{spejimas}\" anksciau");
+                    continue;
+                }
+                else if (neraRaidziuZodije.Contains(spejimas))
+                {
+                    Console.WriteLine($"Jau bandita raide \"{spejimas}\"");
+                    continue;
+                }
+                if (suliginimas.Contains(spejimas))
+                {
+                    atspetosRaides.Add(spejimas);
+
+                    for (int i = 0; i < randomZodis.Length; i++)
+                    {
+                        if (suliginimas[i] == spejimas)
+                        {
+                            pasleptasZodis[i] = randomZodis[i] ;
+                            atskleistosRaides++;
+                        }
+                    }
+                    if (atskleistosRaides == randomZodis.Length)
+                        win = true;
+                }
+                else
+                {
+                    neraRaidziuZodije.Add(spejimas);
+                    Console.WriteLine($"\"{spejimas}\" nera sitame zodije");
+                    spejimai--;
+                    Console.WriteLine($"{spejimai} bandimu liko");
+                }
+                Console.WriteLine(pasleptasZodis.ToString());
+            }
+            Rezultatas(win, randomZodis);
+            Console.ReadKey();
+            Reset();
+            Console.Clear();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Pasirinkite tema:"
+                            + "\n-----------------------------------"
+                            + "\n1. VARDAI"
+                            + "\n2. LIETUVOS MIESTAI"
+                            + "\n3. VALSTYBES"
+                            + "\n4. KITA");
+        }
+        public static string RandomZodis(string[] sarasas)
+        {
+            Random rand = new Random();
+            string randomZodis = sarasas[rand.Next(0, sarasas.Length)];
+            return randomZodis;
+        }
+        public static void Rezultatas(bool veiksmas, string zodis)
+        {
+
+            if (veiksmas)
+            {
+                Console.WriteLine($"Jus atspejot!");
+
+            }
+            else
+            {
+                Console.WriteLine($"Jus pralaimejot! \n Zodis yra {zodis}");
+            }
+
+        }
+        public static string PasleptasZodis(string zodis)
+        {
+            StringBuilder pasleptasZodis = new StringBuilder(zodis.Length);
+            for (int i = 0; i < zodis.Length; i++)
+            {
+                pasleptasZodis.Append('-');
+            }
+            return zodis.ToString();
+        }
+
+        public static void Reset()
+        {
+            atskleistosRaides = 0;
+            spejimas = '\0';
+            win = false;
+            atspetosRaides = new List<char>();
+            neraRaidziuZodije = new List<char>();
+            input = string.Empty;
+            spejimai = 7;
         }
     }
+}
 
